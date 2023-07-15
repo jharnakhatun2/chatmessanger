f996a6a289a058f637e3af1f11f4217dc5954f3b
sohanemon <m.sohanemon@gmail.com>
2023-07-16 00:14:08 +0600

    feat: fix color

--- app/(home)/hero.tsx
+++ app/(home)/hero.tsx
@@ -1,29 +1,29 @@
-import Image from "next/image";
-import Searchbar from "./searchbar";
+import Image from 'next/image';
+import Searchbar from './searchbar';
 
 export default function Hero() {
   return (
-    <div className="container ">
-      <div className="items-center lg:flex flex-row-reverse">
-        <div className="flex items-center justify-center w-full lg:w-1/2">
+    <div className='container '>
+      <div className='flex-row-reverse items-center lg:flex'>
+        <div className='flex items-center justify-center w-full lg:w-1/2'>
           <Image
-            className="w-full h-full"
-            src="/assets/images/image 40.png"
+            className='w-full h-full'
+            src='/assets/images/image 40.png'
             width={923}
             height={899}
-            alt=""
+            alt=''
           />
         </div>
-        <div className="w-full lg:w-1/2 lg:pl-16">
-          <div className="flex ">
-            <h1 className="text-3xl font-semibold text-[#53CCD1] dark:text-white lg:text-[4rem] leading-none">
+        <div className='w-full lg:w-1/2 lg:pl-16'>
+          <div className='flex '>
+            <h1 className='text-3xl font-semibold text-[#53CCD1] dark:text-white lg:text-[4rem] leading-none'>
               Alla
-              <span className="text-[#404040]"> konsultuppdrag samlade </span>
+              <span className='text-[#404040]'> konsultuppdrag samlade </span>
               på en sida
             </h1>
           </div>
-          <div className="bg-primary z-20">
-            <h1 className="text-primary text-7xl">Hello</h1>
+          <div className='z-20 bg-primary'>
+            <h1 className=' text-7xl'>Hello</h1>
             <Searchbar />
           </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,41 +1,41 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class"],
-  content: ["app/**/*.{ts,tsx}", "components/**/*.{ts,tsx}"],
+  darkMode: ['class'],
+  content: ['app/**/*.{ts,tsx}', 'components/**/*.{ts,tsx}'],
   theme: {
     container: {
       center: true,
-      padding: "2rem",
+      padding: '2rem',
       screens: {
-        "2xl": "1400px",
+        '2xl': '1400px',
       },
     },
     extend: {
       fontFamily: {
-        ibm: "var(--IBM)",
+        ibm: 'var(--IBM)',
       },
       colors: {
-        border: "hsl(var(--border))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
+        border: 'hsl(var(--border))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
         primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
         },
         secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
         },
         muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
         },
         accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
         },
       },
     },
   },
-  plugins: [require("tailwindcss-debug-screens")],
+  plugins: [require('tailwindcss-debug-screens')],
 };
