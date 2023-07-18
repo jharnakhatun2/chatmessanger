ae23fda0b9e133bf55c57b05c9fe60b2e71ca2d5
jharnakhatun2 <jharnakhatun203@gmail.com>
2023-07-18 19:20:11 +0600

    added listing desktop

---
+++ components/interfaces.ts
@@ -0,0 +1,10 @@
+export interface dataType {
+  id: string | number;
+  image: string;
+  title: string;
+  name: string;
+  period: string | number;
+  info: string;
+  time: string | number;
+  description?: string;
+}[];
---
+++ components/ui/divider.tsx
@@ -0,0 +1,7 @@
+export default function Divider() {
+  return (
+    <div className="my-0 pt-6">
+      <hr className="h-[2px] bg-accent border-none" />
+    </div>
+  );
+}
