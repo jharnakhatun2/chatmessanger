8f55a4c4f5020ec98d1b3a2714975892394d8d6a
sohanemon <m.sohanemon@gmail.com>
2023-07-21 23:24:22 +0600

    feat: select props

--- app/(home)/searchbar.tsx
+++ app/(home)/searchbar.tsx
@@ -1,25 +1,25 @@
-import Link from "next/link";
-import RadixSelect from "./radix-select";
+import Link from 'next/link';
+import RadixSelect from './radix-select';
 
 export default function Searchbar() {
   return (
-    <div className="bg-white p-3 grid grid-cols-1 lg:grid-cols-3 gap-3 rounded-2xl lg:w-[130%] relative items-center">
+    <div className='bg-white p-3 grid grid-cols-1 lg:grid-cols-3 gap-3 rounded-2xl lg:w-[130%] relative items-center'>
       <div>
-        <RadixSelect />
+        <RadixSelect options={jobSelect} />
       </div>
       <div>
-        <RadixSelect />
+        <RadixSelect options={serviceSelect} />
       </div>
-      <Link href="/listings">
-        <div className="w-full bg-secondary px-24 py-3 rounded-xl text-center">
+      <Link href='/listings'>
+        <div className='w-full px-24 py-3 text-center bg-secondary rounded-xl'>
           Sök
         </div>
       </Link>
     </div>
   );
 }
-export const jobSelect = ["one", "two", "three", "four"];
-export const serviceSelect = ["five", "six", "seven", "eight"];
+export const jobSelect = ['one', 'two', 'three', 'four'];
+export const serviceSelect = ['five', 'six', 'seven', 'eight'];
 
 {
   /* <div className="gap-3 w-[130%] p-2 my-5 bg-white rounded relative z-10 lg:flex">
