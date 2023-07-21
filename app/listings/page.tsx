575401eff5ad2ccd66ed208d136041add0879918
jharnakhatun2 <jharnakhatun203@gmail.com>
2023-07-21 17:55:12 +0600

    added listings responsive

--- app/listings/page.tsx
+++ app/listings/page.tsx
@@ -1,6 +1,8 @@
 import Button from "@/components/ui/button";
 import Divider from "@/components/ui/divider";
-import { ArrowLeft, Search } from "lucide-react";
+import Nav from "@/components/ui/nav";
+import NavMobile from "@/components/ui/nav-mobile";
+import { ArrowLeft, ChevronDown, Search } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -83,81 +85,122 @@ export default function Listing() {
   ];
 
   return (
-    <section className="px-0 lg:px-20 py-10">
-      <div className="container">
-        <Link href="/">
-          <div className="flex items-center cursor-pointer text-primary">
-            <ArrowLeft />
-            <h2 className="text-[1.5rem] font-inter">Meny</h2>
+    <>
+      <Nav />
+      <NavMobile />
+      <section className="px-0 lg:px-20 py-10 hidden lg:flex">
+        <div className="container">
+          <Link href="/">
+            <div className="flex items-center cursor-pointer text-primary">
+              <ArrowLeft />
+              <h2 className="text-[1.5rem] font-inter">Meny</h2>
+            </div>
+          </Link>
+          <Divider />
+          <div className="pt-6 flex flex-wrap w-full gap-3">
+            <div className="relative flex items-center border-2 border-primary rounded-xl font-inter w-full md:w-5/12">
+              <Search className="mx-2" />
+              <input
+                type="text"
+                id="first_name"
+                className="rounded-xl text-foreground pl-3 h-10 bg-white hover:border-none focus:outline-none appearance-none w-full"
+                placeholder="Sök"
+                required
+              />
+            </div>
+            <div className="border-2 border-primary rounded-xl font-inter w-full md:w-3/12">
+              <input
+                type="text"
+                id="first_name"
+                className="rounded-xl text-foreground  h-10 bg-white hover:border-none focus:outline-none appearance-none text-center w-full"
+                placeholder="Stad"
+                required
+              />
+            </div>
+            <div className="border-2 border-primary rounded-xl font-inter w-full md:w-3/12">
+              <input
+                type="text"
+                id="first_name"
+                className="rounded-xl text-foreground h-10 bg-white hover:border-none focus:outline-none appearance-none text-center w-full"
+                placeholder="Yrke"
+                required
+              />
+            </div>
           </div>
-        </Link>
-        <Divider />
-        <div className="pt-6 flex flex-wrap w-full gap-3">
-          <div className="relative flex items-center border-2 border-primary rounded-xl font-inter w-full md:w-5/12">
-            <Search className="mx-2" />
-            <input
-              type="text"
-              id="first_name"
-              className="rounded-xl text-foreground pl-3 h-10 bg-white hover:border-none focus:outline-none appearance-none w-full"
-              placeholder="Sök"
-              required
-            />
-          </div>
-          <div className="border-2 border-primary rounded-xl font-inter w-full md:w-3/12">
-            <input
-              type="text"
-              id="first_name"
-              className="rounded-xl text-foreground  h-10 bg-white hover:border-none focus:outline-none appearance-none text-center w-full"
-              placeholder="Stad"
-              required
-            />
-          </div>
-          <div className="border-2 border-primary rounded-xl font-inter w-full md:w-3/12">
-            <input
-              type="text"
-              id="first_name"
-              className="rounded-xl text-foreground h-10 bg-white hover:border-none focus:outline-none appearance-none text-center w-full"
-              placeholder="Yrke"
-              required
-            />
-          </div>
-        </div>
 
-        <div className="flex flex-col divide-y divide-[#EBEBEB]">
-          {userInfo.map((data, index) => {
-            return (
-              <>
-                <Link href="/listing">
-                  <div
-                    key={index}
-                    className="flex justify-between items-center py-6"
-                  >
-                    <div className="flex gap-5">
-                      <Image
-                        className=""
-                        src={data.image}
-                        width={264}
-                        height={131}
-                        alt=""
-                      />
+          <div className="flex flex-col divide-y divide-[#EBEBEB]">
+            {userInfo.map((data, index) => {
+              return (
+                <>
+                  <Link href="/listing">
+                    <div
+                      key={index}
+                      className="flex justify-between items-center py-6"
+                    >
+                      <div className="flex gap-5">
+                        <Image
+                          className=""
+                          src={data.image}
+                          width={264}
+                          height={131}
+                          alt=""
+                        />
 
-                      <div className="font-inter">
-                        <h2>{data.title}</h2>
-                        <p className="font-bold">{data.period}</p>
-                        <p className="font-bold">{data.info}</p>
-                        <p className="font-bold">{data.time}</p>
+                        <div className="font-inter">
+                          <h2>{data.title}</h2>
+                          <p className="font-bold">{data.period}</p>
+                          <p className="font-bold">{data.info}</p>
+                          <p className="font-bold">{data.time}</p>
+                        </div>
+                      </div>
+                      <div>
+                        <Button outline>Redigera annonsen</Button>
                       </div>
                     </div>
-                    <div>
-                      <Button outline>Redigera annonsen</Button>
-                    </div>
-                  </div>
-                </Link>
-              </>
-            );
-          })}
+                  </Link>
+                </>
+              );
+            })}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <section className="lg:hidden mx-5 h-[1320px]">
+        <div className="pb-5">
+          <div className="bg-accent shadow-lg flex gap-5 justify-center items-center py-2 rounded-2xl">
+            <select className="bg-accent text-xl text-primary px-8 focus:outline-none">
+              <option>Sökfilter</option>
+              <option>Option 2</option>
+              <option>Option 3</option>
+            </select>
+          </div>
+          <p className="font-medium pt-3 text-[#adacac]">
+            198 matchande tjänster.
+          </p>
+        </div>
+        {userInfo.map((item, index) => {
+          return (
+            <Link key={index} href="/listing">
+              <div className="flex justify-between items-center shadow-lg my-10 p-3 rounded-2xl">
+                <div className="font-inter">
+                  <h2 className="text-lg">{item.title}</h2>
+                  <p className="font-semibold text-sm">{item.name}</p>
+                  <p className="font-semibold text-sm">{item.period}</p>
+                  <p className="text-sm">{item.time}</p>
+                  <p className="text-secondary text-sm">Läs mer</p>
+                </div>
+                <div className="flex flex-col justify-center items-center gap-4">
+                  <div className="bg-[#d9d9d9] w-[50px] h-[50px] rounded-full"></div>
+                  <div>
+                    <span className="bg-secondary text-white px-3 py-2 rounded-xl">
+                      Chatta nu
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </Link>
+          );
+        })}
+      </section>
+    </>
   );
 }
